33003ed4beaa678a44aab617b1feb03c75f99683
Hudson Holanda <hudsonholanda55@gmail.com>
2019-10-01 14:36:55 -0300

    Adicionando exemplos de oomentários tanto de linha como comentários de multiplas linhas.

--- javascript/o que é javascript/script.js
+++ javascript/o que é javascript/script.js
@@ -1,10 +1,20 @@
+/* Função: Cria um novo parágrafo e o insere no fim do arquivo */
 function criarParagrafo() {
   var para = document.createElement('p');
   para.textContent = 'Você clicou no botão';
   document.body.appendChild(para);
 }
+/*
+  1. Captura referências de todos os botões da página e armazena isso em um array.
+  2. Vai até todos os botões e adiciona um event listener click a cada um deles.
+*/
 var botoes = document.querySelectorAll('button');
 
 for(var i = 0; i < botoes.length; i++) {
   botoes[i].addEventListener('click', criarParagrafo);
 }
+
+// Eu sou um comentário
+/* Eu também sou 
+   um comentário
+*/
